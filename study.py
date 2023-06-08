fe08e37e22e9e923b65014ec991fd3c245fa17ce
Rajneesh Maurya <112794051+rajneesh-18@users.noreply.github.com>
2023-06-08 07:21:34 +0530

    study.py

--- study.py
+++ study.py
@@ -839,7 +839,28 @@ elif k == r:
 else:
   print("k is greater than r")
 
+# Day 44 Python While loop
+x = 1
+while x < 6:
+  print(x)
+  if (x == 3):
+    break
+  x += 1
 
+x = 0
+while x < 6:
+  x += 1
+  if x == 3:
+    continue
+  print(x)
+  
+x = 1
+while x < 6:
+  print(x)
+  x += 1
+else:
+  print("x is less than 6")
+  
 
 
 
